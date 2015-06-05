7dd7436229ef84a9f407798b7f2fcd92cbd1e1fa
Nataly Tkachuk <techgirlua@gmail.com>
2015-06-05 08:57:21 +0300

    Adding a library route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   root 'pins#index'
 
   resources :pins
-    
+  
+  get 'library' => 'pins#index'  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
